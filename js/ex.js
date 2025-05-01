63ae5d000451cd282b9d4cdd547e792c57882109
Sabino <afonsos@yahoo.com>
2025-05-01 01:30:16 -0300

    vários arquivos

---
+++ js/ex.js
@@ -0,0 +1,19 @@
+var div2_2  = document.createElement('div');
+var h3_div2_2 = document.createElement('h3');
+var ul_div2_2 = document.createElement('ul');
+
+var li1_ul_div2_2 = document.createElement('li');
+
+var li2_ul_div2_2 = document.createElement('li');
+var a_li2_ul_div2_2 = document.createElement('a');
+
+
+var div3 = document.createElement('div');
+var p1_div3 = document.createElement('p');
+var p2_div3= document.createElement('p');
+
+
+var a_li1_ul_div2_1 = document.createElement('a');
+var a_li2_ul_div2_1 = document.createElement('a');
+var a_li3_ul_div2_1 = document.createElement('a');
+var a_li4_ul_div2_1 = document.createElement('a');
