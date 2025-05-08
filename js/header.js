d2704a3870b769ac57d013d038ea7ba3f98f6dbe
Sabino <afonsos@yahoo.com>
2025-05-08 12:32:21 -0300

    Alterado o caminho de arquivos

--- js/header.js
+++ js/header.js
@@ -4,14 +4,34 @@ class Header extends HTMLElement {
     }
 
     connectedCallback(){
-        this.innerHTML = 
+        
+        var caminho = window.location.pathname
+        var index_page = "/C:/Users/bilso/site_pilates/studio_pilates/index.html"
+
+        if (caminho==index_page){
+            //alert(caminho)
+
+            this.innerHTML = 
             `
             <header class="header">
                 <a href="index.html" class="link">
-                    <img src="../img/logo.png" alt="logomarca">
+                    <img src="img/logo.png" alt="logomarca">
                 </a> 
             </header>   
             `
+
+        } else {
+
+            this.innerHTML = 
+                `
+                <header class="header">
+                    <a href="../index.html" class="link">
+                        <img src="../img/logo.png" alt="logomarca">
+                    </a> 
+                </header>   
+                `
+        }
+        
     }
 }
 
--- js/nav.js
+++ js/nav.js
@@ -4,7 +4,25 @@ class Nav extends HTMLElement {
     }
 
     connectedCallback(){
-        this.innerHTML = 
+
+        var caminho = window.location.pathname
+        var index_page = "/C:/Users/bilso/site_pilates/studio_pilates/index.html"
+
+        if (caminho==index_page) {
+
+            this.innerHTML = 
+            `
+            <nav class="nav">
+                <div class="nav-links">
+                    <a href="templates/about.html" class="nav-link">Sobre</a>
+                    <a href="templates/services.html" class="nav-link">Serviços</a>
+                    <a href="templates/articles.html" class="nav-link">Artigos</a>
+                </div>
+            </nav>
+            `
+        } else {
+
+            this.innerHTML = 
             `
             <nav class="nav">
                 <div class="nav-links">
@@ -14,6 +32,8 @@ class Nav extends HTMLElement {
                 </div>
             </nav>
             `
+        }
+       
     }
 }
 
