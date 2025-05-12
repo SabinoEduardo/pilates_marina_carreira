5c71a7258b953bf9f9f16c357691f7649eab62a6
Sabino <afonsos@yahoo.com>
2025-05-12 12:20:34 -0300

    Endereço editado

--- js/footer.js
+++ js/footer.js
@@ -74,7 +74,7 @@ class Footer extends HTMLElement {
 
                     <div class="endereco">
                         <h3>Endereço</h3>
-                        <p>Avenida Ageo Pio Sobrinho, 261, Sala 407 Bairro Buritis, Belo Horizonte - MG</p>
+                        <p>Avenida Aggeo Pio Sobrinho, 261, Sala 407 Bairro Buritis, Belo Horizonte - MG</p>
 
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3749.787084878894!2d-43.97981332388414!3d-19.975454439727375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6bd7e76e2984d%3A0x3db800dbbc9375b7!2sAv.%20Aggeo%20Pio%20Sobrinho%2C%20261%20-%20Buritis%2C%20Belo%20Horizonte%20-%20MG%2C%2030575-834!5e0!3m2!1spt-BR!2sbr!4v1746057323102!5m2!1spt-BR!2sbr" width="800" height="200" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
